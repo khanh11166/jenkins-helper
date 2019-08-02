182ea0b71f85f2ce9ef35e39ce9ec1bd72b2ee70
AAVN\vdkhanh <khanh.vo@axonactive.com>
2019-08-02 17:24:37 +0700

    Add script

--- helper.user.js
+++ helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jenkins Helper
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  try to take over the world!
 // @author       DK
 // @include      /^http.?://.*build.axonivy.io.*$/
